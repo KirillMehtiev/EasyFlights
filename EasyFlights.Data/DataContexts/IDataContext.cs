e1339469da91e6628ecaeafa85a0f2d369850ac1
LDan1117 <gena.sakhnyuk@yandex.ru>
2017-03-17 19:10:02 +0200

    Added base repository. Moved Identity dbcontext to Data.

---
+++ EasyFlights.Data/DataContexts/IDataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace EasyFlights.Data.DataContexts
+{
+    public interface IDataContext : IDisposable
+    {
+        int SaveChanges();
+
+        DbSet<T> Set<T>() where T : class;
+    }
+}
---
+++ EasyFlights.Data/Repositories/Base/Repository.cs
@@ -0,0 +1,35 @@
+﻿using EasyFlights.Data.DataContexts;
+using System.Linq;
+
+namespace EasyFlights.Data.Repositories.Base
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private IDataContext _dataContext;
+
+        public Repository(IDataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        public void Add(T entity)
+        {
+            this._dataContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this._dataContext.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return this._dataContext.Set<T>().AsQueryable();
+        }
+
+        public void SaveChanges()
+        {
+            this._dataContext.SaveChanges();
+        }
+    }
+}
