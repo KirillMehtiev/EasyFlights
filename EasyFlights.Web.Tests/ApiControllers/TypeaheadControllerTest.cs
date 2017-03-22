7ac0b0310471fdaba77727221af260630be97e10
Vladyslav.Biletskyi <vladyslav.biletskyi@nure.ua>
2017-03-22 13:35:20 +0200

    setup unit tests

---
+++ EasyFlights.Web.Tests/ApiControllers/TypeaheadControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EasyFlights.DomainModel.Entities;
+using EasyFlights.Services.Interfaces;
+using EasyFlights.WebApi.ApiControllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EasyFlights.WebApi.Tests.ApiControllers
+{
+    [TestClass]
+    public class TypeaheadControllerTest
+    {
+        [TestMethod]
+        public void GetCitiesForTypeaheadCorrect()
+        {
+            var mock = new Mock<ITypeaheadProvider<City>>();
+            mock.Setup(x => x.GetTypeahead("Nam")).Returns(new List<City>() { new City() { Name = "Name" } });
+            var controller = new TypeaheadController(mock.Object);
+            var expected = "Name";
+            List<City> result = controller.GetCitiesForTypeahead("Nam");
+            Assert.IsNotNull(result.FirstOrDefault());
+            Assert.AreEqual(expected, result.FirstOrDefault()?.Name);
+        }
+    }
+}
