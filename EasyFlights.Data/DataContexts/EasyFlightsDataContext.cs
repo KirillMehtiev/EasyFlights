187b6027eb1f58f04c61d36cac69d80140b5ce1b
LDan1117 <gena.sakhnyuk@yandex.ru>
2017-03-22 17:05:53 +0200

    Task #10. Code style fixes.

--- EasyFlights.Data/DataContexts/EasyFlightsDataContext.cs
+++ EasyFlights.Data/DataContexts/EasyFlightsDataContext.cs
@@ -1,13 +1,12 @@
-﻿namespace EasyFlights.Data.DataContexts
-{
-    using System.Data.Entity;
-    using System.Reflection;
-    using EasyFlights.DomainModel.Entities.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System.Data.Entity;
+using System.Reflection;
+using EasyFlights.DomainModel.Entities.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
+namespace EasyFlights.Data.DataContexts
+{
     public class EasyFlightsDataContext : IdentityDbContext<ApplicationUser>, IDataContext
     {
-        // TODO: add connection to db
         public EasyFlightsDataContext() : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
