6a010f68ed961bd42ecddc41cc64df0eb89c6b18
Irina.Kozheshkurt <Irina.Kozheshkurt@TRN35>
2017-03-22 16:23:29 +0200

    dropped js-files

--- EasyFlights.Web/Client/App/SearchResults/Sorting/SortingComponent.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ko = require("knockout");
-ko.components.register('sorting', {
-    viewModel: require('./SortingViewModel'),
-    template: require('./SortingTemplate.html')
-});
-//# sourceMappingURL=SortingComponent.js.map
