d51607df256c065db0da89accc863ef6975caeb2
vladyslav.biletskyi <vladyslav.biletskyi@nure.ua>
2017-03-23 17:53:41 +0200

    Classes initialized

---
+++ EasyFlights.Data/Migrations/Seed/AircraftsCSVSeeder.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using EasyFlights.Data.DataContexts;
+using EasyFlights.DomainModel.Entities;
+
+namespace EasyFlights.Data.Migrations.Seed
+{
+    public class AircraftsCsvSeeder
+    {
+        public const int ModelIndex = 0;
+        public const int CapacityIndex = 1;
+
+        public void Seed(IDataContext context)
+        {
+            if (context.Set<Aircraft>().Any())
+            {
+                return;
+            }
+
+        }
+    }
+}
---
+++ EasyFlights.Data/Migrations/Seed/FlightsCSVSeeder.cs
@@ -0,0 +1,13 @@
+﻿using EasyFlights.Data.DataContexts;
+
+namespace EasyFlights.Data.Migrations.Seed
+{
+
+    public class FlightsCsvSeeder
+    {
+        public void Seed(IDataContext context)
+        {
+            
+        }
+    }
+}
