9217b1274a0571aff28f34e57bd15616470bbaab
Kirill Mehtiev <kirill.mehtiev@gmail.com>
2017-03-30 11:54:53 +0300

    #34 Finished with inputes for passenger information page

--- EasyFlights.Web/Client/App/SelectFlow/PassengerInfo/PassengerInfoViewModel.ts
+++ EasyFlights.Web/Client/App/SelectFlow/PassengerInfo/PassengerInfoViewModel.ts
@@ -7,15 +7,25 @@ class AgeType {
     static child = "Child";
 }
 
+class SexType {
+    static male = "Male";
+    static female = "Female";
+}
+
 class PassengerInfoViewModel {
     private passengerInfoList: KnockoutObservableArray<PassengerInfoItem>;
     private ageOptions: Array<RadioChooserItem>;
+    private sexOptions: Array<RadioChooserItem>;
 
     constructor(options: IPassengerInfoOptions) {
         this.passengerInfoList = options.passangerInfoList;
         this.ageOptions = [
-            new RadioChooserItem("Adult (18 years and more)", "Adult"),
-            new RadioChooserItem("Child (Under 18 years)", "Child")
+            new RadioChooserItem("Adult (18 years and more)", AgeType.adult),
+            new RadioChooserItem("Child (Under 18 years)", AgeType.child)
+        ];
+        this.sexOptions = [
+            new RadioChooserItem(SexType.male, SexType.male),
+            new RadioChooserItem(SexType.female, SexType.female)
         ];
     }
 }
