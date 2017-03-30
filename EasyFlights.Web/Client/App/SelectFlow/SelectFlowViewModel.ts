b9b6b0754ece0944e98a7d05408973abd0fe4542
vladyslav.biletskyi <vladyslav.biletskyi@nure.ua>
2017-03-30 18:41:33 +0300

    added method call into Select Flow constructor.

--- EasyFlights.Web/Client/App/SelectFlow/SelectFlowViewModel.ts
+++ EasyFlights.Web/Client/App/SelectFlow/SelectFlowViewModel.ts
@@ -1,18 +1,20 @@
 ﻿import ko = require("knockout");
 import { PassengerInfoItem } from "./PassengerInfo/PassengerInfoItem"
 import {FlightItem} from "../SearchResults/FlightResults/Tickets/FlightItem";
+import {DataService} from "../Common/Services/dataService";
 
 class SelectFlowViewModel {
     public routeId: KnockoutObservable<string>;
     public numberOfPassenger: KnockoutObservable<number>;
     public passengerInfoList: KnockoutObservableArray<PassengerInfoItem>;
-    public flights:KnockoutObservableArray<FlightItem>;
+    public flights: KnockoutObservableArray<FlightItem>;
+    private dataService: DataService;
 
     constructor(params) {
         this.routeId = params.routeId;
         this.numberOfPassenger = params.numberOfPassenger;
         this.passengerInfoList = ko.observableArray(this.initPassengerInfoList(1));
-        this.flights = params.flights;
+        this.fillFlightsList(params.routeId);
     }
 
     private initPassengerInfoList(numberOfPassenger: number): Array<PassengerInfoItem> {
@@ -23,6 +25,10 @@ class SelectFlowViewModel {
         }
 
         return result;
+    }
+    private fillFlightsList(routeId: string): void {
+             this.dataService.get<Array<FlightItem>>("api/Flights/Get/".concat(routeId))
+            .then((data) => this.flights(data));       
     }
 }
 
--- EasyFlights.Web/Client/App/SelectFlow/TicketInfo/TicketInfoItem.ts
+++ EasyFlights.Web/Client/App/SelectFlow/TicketInfo/TicketInfoItem.ts
@@ -1,15 +1,16 @@
-﻿import { PassengerInfoItem } from "../PassengerInfo/PassengerInfoItem";
+﻿import ko = require("knockout");
+import { PassengerInfoItem } from "../PassengerInfo/PassengerInfoItem";
 
 export class TicketInfoItem {   
     passenger: PassengerInfoItem;
     ticketNumber: number;
-    ticketClass: string;
-    seat: number;
-    price:number;
+    ticketClass: KnockoutObservable<string>;
+    seat: KnockoutObservable<number>;
+    price: KnockoutObservable<number>;
     constructor(passenger:PassengerInfoItem, ticketNumber: number, ticketClass: string, seat: number, price: number) {
-        this.price = price;
-        this.seat = seat;
-        this.ticketClass = ticketClass;
+        this.price = ko.observable(price);
+        this.seat = ko.observable(seat);
+        this.ticketClass = ko.observable(ticketClass);
         this.ticketNumber = ticketNumber;
         this.passenger = passenger;
     }
