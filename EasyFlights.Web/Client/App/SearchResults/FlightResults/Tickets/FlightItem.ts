b6500dae152870e6a6a132cc32d312878a687a16
vladyslav.biletskyi <vladyslav.biletskyi@nure.ua>
2017-03-30 14:44:24 +0300

    Template released

--- EasyFlights.Web/Client/App/SearchResults/FlightResults/Tickets/FlightItem.ts
+++ EasyFlights.Web/Client/App/SearchResults/FlightResults/Tickets/FlightItem.ts
@@ -1,17 +1,17 @@
 ﻿import ko = require("knockout");
+import {TicketInfoItem} from "../../../SelectFlow/TicketInfo/TicketInfoItem";
 
 export class FlightItem {
     //public id: number;
     public arrivalTime: string;
     public departureAirport: string;
     public departureTime: string;
-    public destinationAirport: string;
-    
-    
+    public destinationAirport: string;    
     public duration: string;
     public fare: string;
+    public tickets:KnockoutObservableArray<TicketInfoItem>;
 
-    constructor(id: number, departureAirport: string, destinationAirport: string, departureTime: string,  arrivalTime: string, duration: string, fare:string) {
+    constructor(id: number, departureAirport: string, destinationAirport: string, departureTime: string, arrivalTime: string, duration: string, fare: string, tickets: KnockoutObservableArray<TicketInfoItem>) {
         //this.id = id;
         this.departureAirport = departureAirport;
         this.destinationAirport = destinationAirport;
@@ -19,5 +19,6 @@ export class FlightItem {
         this.arrivalTime = arrivalTime; 
         this.duration = duration;
         this.fare = fare;
+        this.tickets = tickets;
     }
 }
--- EasyFlights.Web/Client/App/SelectFlow/TicketInfo/TicketInfoViewModel.ts
+++ EasyFlights.Web/Client/App/SelectFlow/TicketInfo/TicketInfoViewModel.ts
@@ -1,14 +1,20 @@
 ﻿import {TicketInfoItem} from "./TicketInfoItem";
 import { ITicketInfoItemOptions } from "./ITicketInfoItemOptions";
 import { PassengerInfoItem } from "../PassengerInfo/PassengerInfoItem";
+import {RadioChooserItem} from "../../Common/Components/RadioChooser/RadioChooserItem";
 
 class TicketInfoViewModel {
     public item: TicketInfoItem;
     public passengers: KnockoutObservableArray<PassengerInfoItem>;
-    public test: KnockoutObservable<string>;
+    public classType:Array<RadioChooserItem>;
 
     constructor(options: ITicketInfoItemOptions) {
         this.passengers = options.passengers;
+        this.classType = [
+            new RadioChooserItem("Economy", "Economy"),
+            new RadioChooserItem("Business", "Business"),
+            new RadioChooserItem("First", "First")
+        ];
     }    
 }
 export = TicketInfoViewModel;
