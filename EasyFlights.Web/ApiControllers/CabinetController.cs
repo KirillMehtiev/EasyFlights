5a8cf5de0f6293287ec17aa6bc3f6ca98096a215
vladyslav.biletskyi <vladyslav.biletskyi@nure.ua>
2017-04-03 15:07:26 +0300

    [43] Added controller

---
+++ EasyFlights.Web/ApiControllers/CabinetController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace EasyFlights.Web.ApiControllers
+{
+    [RoutePrefix("api/Cabinet")]
+    public class CabinetController : ApiController
+    {
+        [HttpGet]
+        [Route("ValidateEmail")]
+        public bool ValidateEmail([FromUri] string email)
+        {
+            // not implemented yet
+            return true;
+        }
+
+        [HttpPost]
+        [Route("ChangePassword")]
+        public bool ChangePassword(string oldPassword, string newPassword, string newPasswordConfirm)
+        {
+            // not implemented yet
+            return newPassword != oldPassword && newPassword == newPasswordConfirm;
+        }
+    }
+}
