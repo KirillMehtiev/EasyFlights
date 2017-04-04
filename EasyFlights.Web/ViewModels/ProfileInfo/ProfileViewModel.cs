a045c1c0ce2e8b32f9e936573da19306fe7ac0a7
Irina.Kozheshkurt <Irina.Kozheshkurt@TRN35>
2017-04-04 15:53:39 +0300

    [44] - User profile - add View Model

---
+++ EasyFlights.Web/ViewModels/ProfileInfo/ProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyFlights.Web.ViewModels.ProfileInfo
+{
+    public class ProfileViewModel
+    {
+       public string FirstName { get; set; }
+
+       public string LastName { get; set; }
+
+       public string ContactPhone { get; set; }
+
+       public string Sex { get; set; }
+
+       public string DateOfBirth { get; set; }
+    }
+}
