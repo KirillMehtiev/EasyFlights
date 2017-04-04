8b71191e8e7b6dc526545145d8eb97b5ad983f20
Irina.Kozheshkurt <Irina.Kozheshkurt@TRN35>
2017-04-04 15:48:19 +0300

    [44] UI:Cabinet-> Profile info - add profileInfo Item

---
+++ EasyFlights.Web/Client/App/UserCabinet/ProfileInfo/ProfileInfoItem.ts
@@ -0,0 +1,18 @@
+﻿import ko = require("knockout");
+
+export class ProfileInfoItem {
+   
+    public firstName: string;
+    public lastName: string;
+    public birthday: string;
+    public sex: string;
+    public contactPhone: string;
+
+    constructor(firstName: string, lastName: string, birthday: string, sex: string, contactPhone: string ) {
+        this.contactPhone = contactPhone;
+        this.sex = sex;
+        this.birthday = birthday;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+}
