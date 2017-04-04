3ec88cee3cfaeaba9871876eb1eece75d5641a6b
vladyslav.biletskyi <vladyslav.biletskyi@nure.ua>
2017-04-04 18:45:08 +0300

    [14] released unit tests.

--- EasyFlights.Services.Tests/Services/Flight/FlightServiceTest.cs
+++ EasyFlights.Services.Tests/Services/Flight/FlightServiceTest.cs
@@ -1,15 +1,17 @@
-﻿using EasyFlights.Data.Repositories.Flights;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EasyFlights.Services.Tests.Services.Flight
 {
     [TestClass]
     public class FlightServiceTest
     {
-
         [TestMethod]
         public void GetCabin()
+        {
+        }
+
+        [TestMethod]
+        public void GetAvailableSeats()
         {
         }
     }
