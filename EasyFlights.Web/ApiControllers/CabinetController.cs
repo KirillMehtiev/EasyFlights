c28c9b89b0d9dda918271feccf02a3b0439b46a2
vladyslav.biletskyi <vladyslav.biletskyi@nure.ua>
2017-04-06 12:43:17 +0300

    [43] Changing password is now working

--- EasyFlights.Web/ApiControllers/CabinetController.cs
+++ EasyFlights.Web/ApiControllers/CabinetController.cs
@@ -1,26 +1,52 @@
-﻿using System.Web.Helpers;
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
 using System.Web.Http;
+using EasyFlights.DomainModel.Entities.Identity;
+using EasyFlights.Web.Infrastracture;
 using EasyFlights.Web.ViewModels.AccountViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace EasyFlights.Web.ApiControllers
 {
     [RoutePrefix("api/Cabinet")]
     public class CabinetController : ApiController
     {
+        private ApplicationUserManager userManager;
+
+        public ApplicationUserManager UserManager => userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+
         [HttpGet]
+        [Authorize]
         [Route("ValidateEmail")]
-        public bool ValidateEmail([FromUri] string email)
+        public async Task<bool> ValidateEmailAsync([FromUri] string email)
         {
-            // not implemented yet
-            return true;
+            ApplicationUser user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            return user.Email == email;
         }
 
         [HttpPost]
+        [Authorize]
         [Route("ChangePassword")]
         public bool ChangePassword([FromBody] ChangePasswordViewModel model)
         {
-            // not implemented yet
-            return model.NewPassword != model.OldPassword && model.NewPassword == model.NewPasswordConfirm;
+            if (!ModelState.IsValid)
+            {
+                return false;
+            }
+            ApplicationUser user = UserManager.FindById(User.Identity.GetUserId());
+            user.PasswordHash = UserManager.PasswordHasher.HashPassword(model.NewPassword);
+            IdentityResult result = UserManager.Update(user);
+            if (!result.Succeeded)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
