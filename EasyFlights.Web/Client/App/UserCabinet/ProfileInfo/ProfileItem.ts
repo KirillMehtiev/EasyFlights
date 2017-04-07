489ec1e0ed2de6102d8c37b9542a45d6b495ac61
Irina <Irina@IRINA>
2017-04-07 12:19:28 +0300

    [44] profile cabinet

---
+++ EasyFlights.Web/Client/App/UserCabinet/ProfileInfo/ProfileItem.ts
@@ -0,0 +1,18 @@
+﻿export class ProfileItem {
+
+    public firstName: KnockoutObservable<string>;
+    public lastName: KnockoutObservable<string>;
+    public birthday: KnockoutObservable<string>;
+    public sex: KnockoutObservable<string>;
+    public contactPhone: KnockoutObservable<string>;
+    public email: KnockoutObservable<string>;
+
+    constructor(firstName: KnockoutObservable<string>, lastName: KnockoutObservable<string>, birthday: KnockoutObservable<string>, sex: KnockoutObservable<string>, contactPhone: KnockoutObservable<string>, email: KnockoutObservable<string>) {
+        this.contactPhone = contactPhone;
+        this.sex = sex;
+        this.birthday = birthday;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+}
