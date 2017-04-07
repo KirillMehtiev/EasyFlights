58965bbd82a870fe9c534b10ed012df59c924908
Gennadiy.Sakhnyuk <Gennadiy.Sakhnyuk@TRN31>
2017-04-07 16:54:12 +0300

    [88] [Order summary: convert sex code into string] [Bug was fixed]

--- EasyFlights.Web/Client/App/TicketsFlow/BaseComponents/OrderSummary/Ticket/TicketViewModel.ts
+++ EasyFlights.Web/Client/App/TicketsFlow/BaseComponents/OrderSummary/Ticket/TicketViewModel.ts
@@ -1,14 +1,30 @@
-﻿import { ITicketOptions } from "./ITicketOptions";
+﻿import ko = require("knockout");
+import { ITicketOptions } from "./ITicketOptions";
 import { IEditablePassengerOptions } from "../../PassengersInfo/EditablePassenger/IEditablePassengerOptions";
 import { IEditableTicketOptions } from "../../TicketInfo/EditableTicket/IEditableTicketOptions";
+import { Sex } from "../../../../Common/Enum/Enums";
 
 class TicketViewModel {
     public passenger: IEditablePassengerOptions;
     public ticket: IEditableTicketOptions;
+    public passengerSex: KnockoutComputed<string>;
 
     constructor(options: ITicketOptions) {
         this.passenger = options.passenger;
         this.ticket = options.passenger.tickets()[options.ticketIndex()];
+
+        this.passengerSex = ko.computed(() => this.convertPassengerSexToString());
+    }
+
+    public convertPassengerSexToString() {
+        switch (this.passenger.sex()) {
+            case Sex.Male:
+                return "Male";
+            case Sex.Female:
+                return "Female";
+            default:
+                return "Unknown gender";
+        }
     }
 }
 
